b6d7a5bc54c39dd6834eb56c86a214fead251641
Matěj Beran <matej.beran96@gmail.com>
2020-07-05 16:26:18 +0200

    Programmatically snap Cube to the game grid.

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+
+    void Update()
+    {
+        float x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        float z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(x, 0f, z);
+    }
+}
