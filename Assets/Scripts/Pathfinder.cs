a3e7673438a639c2ca29f22524763d37c5c179a7
Matěj Beran <matej.beran96@gmail.com>
2020-07-18 13:47:30 +0200

    Pathfinder init.

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    void Start()
+    {
+        LoadWaypoints();
+    }
+
+    private void LoadWaypoints()
+    {
+        Waypoint[] waypoints = FindObjectsOfType<Waypoint>();
+        foreach (Waypoint waypoint in waypoints)
+        {
+            Vector2Int gridPos = waypoint.GetGridPosition();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Waypoint " + waypoint.name + " is overlapping. Skipping ...");
+                continue;
+            }
+
+            grid.Add(gridPos, waypoint);
+        }
+    }
+}
