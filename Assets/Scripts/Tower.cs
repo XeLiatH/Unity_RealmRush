403f579b525aed3e9aae57c30b7295ffc121eb17
Matěj Beran <matej.beran96@gmail.com>
2020-07-25 19:25:17 +0200

    Tower look at enemies.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtEnemy();
+    }
+
+    private void LookAtEnemy()
+    {
+        objectToPan.LookAt(target);
+    }
+}
